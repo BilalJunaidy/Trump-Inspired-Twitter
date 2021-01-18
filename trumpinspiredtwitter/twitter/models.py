3dd5430922e0cfbdd4851bba29607dd81f182fdf
Bilal Junaidy <bilal.junaidy@gmail.com>
2021-01-18 16:34:21 +0500

    Bootstrap added

--- trumpinspiredtwitter/twitter/models.py
+++ trumpinspiredtwitter/twitter/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Tweet(models.Model):
-    content = models.TextField(blank=True, null=True)
+    content = models.TextField(max_length = 140)
     image = models.ImageField(upload_to = 'images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
